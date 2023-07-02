cd94e118c279fe493a01443f5f2b0a7e6a2277c0
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-07-02 20:26:23 +0700

    Locate the node which will be the parent of the node to be inserted

--- inarysearchtree/inarysearchtree.cpp
+++ inarysearchtree/inarysearchtree.cpp
@@ -32,7 +32,7 @@ public:
 
 		Node* parent = NULL;
 		Node* currentNode = NULL;
-		search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
+		search(element, parent, currentNode);  // Locate the node which will be the parent of the node to be inserted
 
 		if (parent == NULL) // If the parent is NULL (Tree is empty)
 		{
